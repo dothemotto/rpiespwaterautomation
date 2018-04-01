2bd0fa2bf0e697ed5ab5beaf16e792468c1fbcee
Laurentiu Motoonu <motto_laur_costi@yahoo.com>
2018-04-01 22:08:09 +0300

    Pi4J setup

---
+++ water-automation/src/main/java/nu/motoo/waterautomation/controller/LedController.java
@@ -0,0 +1,31 @@
+package nu.motoo.waterautomation.controller;
+
+import com.pi4j.io.gpio.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LedController {
+
+    private static GpioPinDigitalOutput pin;
+
+    @RequestMapping("/light")
+    public String lightTheLed() {
+        try {
+            if (pin == null) {
+                GpioController controller = GpioFactory.getInstance();
+                pin = controller.provisionDigitalOutputPin(
+                        RaspiPin.GPIO_04,
+                        "LED",
+                        PinState.LOW
+                );
+            }
+
+            pin.toggle();
+
+            return "LED lighted";
+        } catch (Exception e) {
+            return "LED cannot be lighted! There are some problems with RPI connection: " + e.getMessage();
+        }
+    }
+}
